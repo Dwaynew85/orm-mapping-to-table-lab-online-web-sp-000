8d053f793ea9a0db75c811e23a53bd5914a45fc4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-02 21:51:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,17 @@ class Student
     @id = id
   end
 
+  self.create_table
+    sql <<- SQL 
+    " CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    grade INTEGER
+    );"
+    SQL 
+    DB[:conn]
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
